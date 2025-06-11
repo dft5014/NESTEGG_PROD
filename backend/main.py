57e3c6bee9595abc88ed1758ea4d863cf0a6eca8
dft5014 <daptventures@gmail.com>
2025-06-11 12:20:56 -0400

    k

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ import jwt
 import statistics
 import sqlalchemy
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends, HTTPException, status, Query, File
+from fastapi import FastAPI, Depends, HTTPException, status, Query, File, UploadFile, Form, Response, BackgroundTasks, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
