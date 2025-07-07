9fb20ca1d4e734c81be0552510984c18b07bd2e4
dft5014 <daptventures@gmail.com>
2025-07-07 16:48:02 -0400

    test fix

---
+++ frontend/store/hooks/index.js
@@ -0,0 +1,4 @@
+// Export all hooks from a single entry point
+export { usePortfolioSummary } from './usePortfolioSummary';
+export { usePortfolioTrends } from './usePortfolioTrends';
+export { useDataMutations } from './useDataMutations';
